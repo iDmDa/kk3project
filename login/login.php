913f139ae297223b47f10bb33b14704f4bcdf398
DmDa <uloga_d@hotmail.com>
2024-10-29 03:07:48 +0300

    login react remake

--- login/login.php
+++ login/login.php
@@ -1,13 +1,36 @@
 <?php
 
-$password = "vhod";
+$logResult = login($_POST['log'], $_POST['pass']);
+
+if($_SESSION['login'] != "login" and $logResult == 1) {
+    $_SESSION['login'] = "login";
+}
 
 if($_SESSION['login'] != "login") {
     require("index.html");
-    if($_POST['pass'] != md5($password)) exit;
-    $_SESSION['login'] = "login";
-    header("Refresh:0");
     exit;
+}
+
+function login($login, $pass) {
+    include("dbdata.php");
+
+    try {
+        $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_login, $db_pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+        $sql = "SELECT * FROM users WHERE login ='{$login}' and pass = '{$pass}'";
+    
+        $result = $pdo->query($sql);
+        $rowCount = $result->rowCount();
+        //$rows = $result->fetchAll(PDO::FETCH_ASSOC);
+        //$rows = $result->fetchAll();
+    
+        if($rowCount) return 1;
+        return 0;
+        
+    } catch (PDOException $e) {
+        echo "Ошибка подключения: " . $e->getMessage();
+    }
 }
 
 ?>
